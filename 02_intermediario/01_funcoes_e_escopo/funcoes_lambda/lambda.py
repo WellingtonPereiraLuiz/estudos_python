55c41ef8b80896b813cb17b6c8a85ef0a2f25f7b
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2025-08-21 17:21:58 -0400

    Praticando listas e maps com lambda
    Apenas aplicando alguns conceitos basicos e usando o chat para me explicar. Ainda estou tentando entender a real diferenca entre eles e o uso do generator

--- 02_intermediario/01_funcoes_e_escopo/funcoes_lambda/lambda.py
+++ 02_intermediario/01_funcoes_e_escopo/funcoes_lambda/lambda.py
@@ -4,7 +4,19 @@ Lambda em Python
 Este exemplo demonstra como usar funções lambda para criar funções anônimas e simples.
 """
 
-# Como criar uma função lambda simples
-soma_lambda = lambda x, y: x + y
-resultado_lambda = soma_lambda(5, 3)
-print(f"A soma usando lambda é: {resultado_lambda}")
+# # Como criar uma função lambda simples
+# soma_lambda = lambda x, y: x + y
+# resultado_lambda = soma_lambda(5, 3)
+# print(f"A soma usando lambda é: {resultado_lambda}")
+
+
+# # Como usar uma função lambda com lista
+# x = [1, 2, 3, 4, 5]
+# a = lambda lista: (x + 1 if x >= 3 else x for x in lista)
+# print(list(a(x)))
+
+
+# Como usar uma função lambda com map
+numeros = [1, 2, 3, 4, 5]
+numeros_incrementados = list(map(lambda x: x + 1, numeros))
+print(f"Números incrementados: {numeros_incrementados}")
