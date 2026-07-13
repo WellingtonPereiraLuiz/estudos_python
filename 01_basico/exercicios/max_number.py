cefcf66353844f4ea83eebae8f7821c38b61baac
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2026-07-13 09:14:49 -0400

    feat: exercicio maior de tres numeros sem max()

--- 01_basico/exercicios/max_number.py
+++ 01_basico/exercicios/max_number.py
@@ -1,12 +1,12 @@
 contador = 0
-max_number = 0
+max_number = float('-inf')
 while contador < 3:
     number_entry = float(input("Digite um number aleatorio: "))
     contador += 1
     if number_entry > max_number:
-        print(f"O numero digitado e maior ao anterior Digitado: {number_entry} | Anterior: {max_number}")
+        print(f"O numero digitado e maior ate o momento. Maior numero: {number_entry}")
         max_number = number_entry
     else:
-        print(f"O numero digitado e inferior ao anterior digitado: {number_entry} | Anterior: {max_number}")
+        print(f"O numero digitado não e o maior numero: Numero digitado {number_entry} | Maior numero: {max_number}")
 
    
