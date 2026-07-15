e361466ec919e24295334e0071ec9e20e6047695
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2026-07-15 12:17:30 -0400

    feat: organizando logica

--- 01_basico/exercicios/nota_basic.py
+++ 01_basico/exercicios/nota_basic.py
@@ -1,10 +1,10 @@
 # Leia uma nota (0–10): ≥7 Aprovado, 5–6.9 Recuperação, <5 Reprovado. (Sem olhar o antigo!)
                                                                      
-nota = int(input("Digite a sua nota: "))
+nota = float(input("Digite a sua nota: "))
 
 if nota >= 7 and nota <= 10:
     print("Aprovado!")
-elif nota < 7 and nota > 5:
+elif nota < 7 and nota >= 5:
     print("Recuperação")
 elif nota < 5 and nota >= 0:
     print("Reprovado")
