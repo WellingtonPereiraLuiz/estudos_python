e17beee094d9ee5ffc9a67064088966e1593872e
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2026-08-26 12:08:38 -0400

    feat: exercicio 1 da fase 0.3 - lista de sabores com append, remove e sort

---
+++ 01_basico/exercicios/lista_de_sabores.py
@@ -0,0 +1,12 @@
+sabores = ['brigadeiro', 'maracuja', 'leite e po']
+
+sabores.append('brisadeiro')
+sabores.append('beijinho')
+sabores.append('castanha')
+
+
+sabores.remove('maracuja')
+
+sabores.sort()
+
+print(sabores)
